0b27f5ebecd5a30f4eb74c050e9314f52195ef96
exohki <142925167+exohki@users.noreply.github.com>
2023-11-02 20:30:50 -0500

    Add files via upload

---
+++ ContextExample/Data/Context.cs
@@ -0,0 +1,42 @@
+﻿using ContextExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextExample.Data
+{
+    public class Context : IContext
+    {
+        public List<Movie> Movies { get; set; }
+
+
+        public Context()
+        {
+            Movies = new List<Movie>()
+            {
+                new Movie { Id = 1, Title = "Toy Story" },
+                new Movie { Id = 2, Title = "Star Wars" },
+                new Movie { Id = 3, Title = "Oceans 11" },
+                new Movie { Id = 3, Title = "Oceans 12" }
+            };
+        }
+
+        public Movie GetById(int id)
+        {
+            return Movies.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Movie GetByTitle(string title)
+        {
+            return Movies.FirstOrDefault(x => x.Title == title);
+        }
+
+        public List<Movie> FindMovie(string title)
+        {
+            // Using LINQ to perform a case insensitive substring search
+            return Movies.Where(m => m.Title.IndexOf(title, StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+        }
+    }
+}
---
+++ ContextExample/Services/MainService.cs
@@ -0,0 +1,87 @@
+﻿using ContextExample.Data;
+using ContextExample.Services;
+using System;
+using System.Linq;
+
+public class MainService : IMainService
+{
+    private readonly IContext _context;
+
+    public MainService(IContext context)
+    {
+        _context = context;
+    }
+
+    public void Invoke()
+    {
+        bool exit = false;
+        while (!exit)
+        {
+            Console.WriteLine("Select an option:");
+            Console.WriteLine("1. Get movie by Id");
+            Console.WriteLine("2. Get movie by title");
+            Console.WriteLine("3. Find list of movies matching title");
+            Console.WriteLine("4. Exit");
+
+            var option = Console.ReadLine();
+            switch (option)
+            {
+                case "1":
+                    Console.WriteLine("Enter movie ID:");
+                    if (int.TryParse(Console.ReadLine(), out int id))
+                    {
+                        var movieById = _context.GetById(id);
+                        if (movieById != null)
+                        {
+                            Console.WriteLine($"Movie found: {movieById.Title}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Movie not found.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid ID format.");
+                    }
+                    break;
+                case "2":
+                    Console.WriteLine("Enter movie title:");
+                    var title = Console.ReadLine();
+                    var movieByTitle = _context.GetByTitle(title);
+                    if (movieByTitle != null)
+                    {
+                        Console.WriteLine($"Movie found: {movieByTitle.Title}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Movie not found.");
+                    }
+                    break;
+                case "3":
+                    Console.WriteLine("Enter search string for movie title:");
+                    var searchString = Console.ReadLine();
+                    var movies = _context.FindMovie(searchString);
+                    if (movies.Any())
+                    {
+                        Console.WriteLine("Movies found:");
+                        foreach (var movie in movies)
+                        {
+                            Console.WriteLine(movie.Title);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No movies found with that title.");
+                    }
+                    break;
+                case "4":
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option, please try again.");
+                    break;
+            }
+        }
+    }
+}
